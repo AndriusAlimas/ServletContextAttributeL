1d9e4405c4333be1205d6a39756d2a5122919ce5
Andrius <andriusjavait@gmail.com>
2017-01-17 20:48:37 +0000

    update DD to have a listener added listener template code

---
+++ ServletContextAttributeL/src/com/example/listener/MyServletContextAttributeListener.java
@@ -0,0 +1,21 @@
+package com.example.listener;
+import javax.servlet.*;
+
+public class MyServletContextAttributeListener implements ServletContextAttributeListener {
+
+@Override
+public void attributeAdded(ServletContextAttributeEvent event) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void attributeRemoved(ServletContextAttributeEvent event) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void attributeReplaced(ServletContextAttributeEvent event) {
+	// TODO Auto-generated method stub
+	
+}
+}
