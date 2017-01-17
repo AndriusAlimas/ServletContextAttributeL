03e3c5d9b5f642ff63311e7bca99d9dd14335703
Andrius <andriusjavait@gmail.com>
2017-01-17 21:01:14 +0000

    update ServletContextListener code with full functionality, you can see result on console

--- ServletContextAttributeL/src/com/example/listener/MyServletContextAttributeListener.java
+++ ServletContextAttributeL/src/com/example/listener/MyServletContextAttributeListener.java
@@ -5,17 +5,30 @@ public class MyServletContextAttributeListener implements ServletContextAttribut
 
 @Override
 public void attributeAdded(ServletContextAttributeEvent event) {
-	// TODO Auto-generated method stub
+	  System.out.println("\n###################################\n");
+      System.out.println("attributeAdded method has been called in" + getClass().getName());
+      // ServletContextAttributeEvent ofcourse you can call getName() and getValue() 
+      // because you talking about attributes:
+      System.out.println("Newly added attribute name = " + event.getName() + 
+    		  ", value = " + event.getValue());
+      System.out.println("\n###################################\n");
 	
 }
 @Override
 public void attributeRemoved(ServletContextAttributeEvent event) {
-	// TODO Auto-generated method stub
-	
+	 System.out.println("\n###################################\n");
+     System.out.println("attributeRemoved method has been called in" + this.getClass().getName());
+     System.out.println("Removed attribute name = " + event.getName() + 
+    		 ", value = " + event.getValue());
+     System.out.println("\n###################################\n");	
 }
 @Override
 public void attributeReplaced(ServletContextAttributeEvent event) {
-	// TODO Auto-generated method stub
+	   System.out.println("\n###################################\n");
+       System.out.println("attributeReplaced method has been called in" + this.getClass().getName());
+       System.out.println("Replaced attribute name = " + event.getName() + 
+    		   ", value = " + event.getValue());
+       System.out.println("\n###################################\n");
 	
-}
+	}
 }
