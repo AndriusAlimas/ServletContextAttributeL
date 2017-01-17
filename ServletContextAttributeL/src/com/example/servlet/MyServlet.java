98e454422927e7097357f182a8bb2d0110f3da25
Andrius <andriusjavait@gmail.com>
2017-01-17 20:56:15 +0000

    Servlet code done, sets of context attribute this example to invoke attribute listener

--- ServletContextAttributeL/src/com/example/servlet/MyServlet.java
+++ ServletContextAttributeL/src/com/example/servlet/MyServlet.java
@@ -6,6 +6,29 @@ import java.io.*;
 public class MyServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, 
 			HttpServletResponse response)throws ServletException,IOException{
-		
+		response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        ServletContext sc = this.getServletContext();
+        // go this thread to sleep, to see in action in slow motion:
+        sleep();
+        // simple servlet context global attribute set in code, not receive,
+        // you will not see anything in browser, just look console for result
+        // this will call event attributeAdded:
+        sc.setAttribute("CompanyName", "Next");
+        sleep();
+        // this will call event attributeReplaced:
+        sc.setAttribute("CompanyName", "Asda");
+        sleep();
+        // this will call event attributeRemoved:
+        sc.setAttribute("CompanyName", null);
 	}
+	// method to put thread sleep :
+	 private void sleep() {
+	        try {
+	            Thread.sleep(9000);
+	        }
+	        catch (InterruptedException ex) {
+	            ex.printStackTrace();
+	        }
+	    }
 }
